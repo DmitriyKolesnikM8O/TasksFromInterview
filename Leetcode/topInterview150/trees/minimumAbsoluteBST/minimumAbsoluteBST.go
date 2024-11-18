dd8a58f176f6c480f3ea0ea23ee68bb4a97a26bb
Dmitriy <kolesnik982011@gmail.com>
2024-11-18 12:00:16 +0300

    minimumADSBST

---
+++ Leetcode/topInterview150/trees/minimumAbsoluteBST/minimumAbsoluteBST.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	// tree := &TreeNode{
+	// 	Val: 4,
+	// 	Left: &TreeNode{
+	// 		Val: 2,
+	// 		Left: &TreeNode{
+	// 			Val:   1,
+	// 			Left:  nil,
+	// 			Right: nil,
+	// 		},
+	// 		Right: &TreeNode{
+	// 			Val:   3,
+	// 			Left:  nil,
+	// 			Right: nil,
+	// 		},
+	// 	},
+	// 	Right: &TreeNode{
+	// 		Val:   6,
+	// 		Right: nil,
+	// 		Left:  nil,
+	// 	},
+	// }
+	// tree := &TreeNode{
+	// 	Val: 1,
+	// 	Left: &TreeNode{
+	// 		Val:   0,
+	// 		Left:  nil,
+	// 		Right: nil,
+	// 	},
+	// 	Right: &TreeNode{
+	// 		Val: 48,
+	// 		Right: &TreeNode{
+	// 			Val:   12,
+	// 			Left:  nil,
+	// 			Right: nil,
+	// 		},
+	// 		Left: &TreeNode{
+	// 			Val:   49,
+	// 			Left:  nil,
+	// 			Right: nil,
+	// 		},
+	// 	},
+	// }
+
+	tree := &TreeNode{
+		Val: 543,
+		Left: &TreeNode{
+			Val:  384,
+			Left: nil,
+			Right: &TreeNode{
+				Val:   445,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+		Right: &TreeNode{
+			Val: 652,
+			Right: &TreeNode{
+				Val:   699,
+				Left:  nil,
+				Right: nil,
+			},
+			Left: nil,
+		},
+	}
+	fmt.Println(getMinimumDifference(tree))
+
+}
+
+func getMinimumDifference(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if root != nil && root.Left == nil && root.Right == nil {
+		return root.Val
+	}
+
+	nodeList := []int{}
+
+	currentNode := []*TreeNode{root}
+
+	for len(currentNode) != 0 {
+		nextNode := []*TreeNode{}
+
+		for _, node := range currentNode {
+			if node != nil {
+				nodeList = append(nodeList, node.Val)
+			}
+
+			if node.Left != nil {
+				nextNode = append(nextNode, node.Left)
+			}
+
+			if node.Right != nil {
+				nextNode = append(nextNode, node.Right)
+			}
+		}
+		currentNode = nextNode
+	}
+	result := int(math.Abs(float64(nodeList[0]) - float64(nodeList[1])))
+	for i := range nodeList {
+		for j := range nodeList {
+
+			if int(math.Abs(float64(nodeList[i])-float64(nodeList[j]))) < result && i != j {
+				result = int(math.Abs(float64(nodeList[i]) - float64(nodeList[j])))
+			}
+		}
+	}
+
+	return result
+}
